ec2cbd745fcdcd3734ce20a787f2285cbe018485
sergioalcocer01 <158301668+sergioalcocer01@users.noreply.github.com>
2026-05-05 18:13:20 +0200

    Gestion directorios

--- DJF/ITs/EDROOMModel/IT_CCDroneMng/service_libraries/edroomsl/edroombp/include/public/edroombp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PUBLIC_EDROOMBP_H_
-#define _PUBLIC_EDROOMBP_H_
-
-#include "../../../../llsw/config/include/public/config.h"
-
-#include	<edroombp_swr/rtemsapi_5_1/rtems_5_1/edroombp.h>
-
-#endif
-
